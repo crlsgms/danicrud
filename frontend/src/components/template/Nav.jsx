4cfe4749af08baa63754e7e9f433935de1a1472a
Carlos Gomes <carlos.henrique@unesp.br>
2020-10-19 11:14:28 -0300

    primeira parte aula 191020

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -12,5 +12,8 @@ export default props =>
             <Link to="/users">
                 <i className="fa fa-users"></i> Usuários
             </Link>
+            <Link to="/products">
+                <i className="fa fa-camera-retro"></i> Produtos
+            </Link>
         </nav>
     </aside>
